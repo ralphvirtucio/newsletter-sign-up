fe430f93da9e7643a5204925048642929b295771
ralphvirtucio <ralphvirtucio3@gmail.com>
2024-07-30 11:18:12 +0800

    TODO: form validation

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-console.log('helloword')
+const form = document.querySelector('#form')
+
+
+function handleSubmit(e) {
+  e.preventDefault();
+
+  const validEndEmail = ['gmail.com', 'outlook.com', 'yahoo.com']
+
+  const formData = new FormData(e.target)
+  const data = Object.fromEntries(formData)
+
+  if(!data.email.includes("@")) {
+      console.log('invalid')
+  } 
+
+  console.log('valid')
+
+}
+
+
+form.addEventListener('submit', handleSubmit)
